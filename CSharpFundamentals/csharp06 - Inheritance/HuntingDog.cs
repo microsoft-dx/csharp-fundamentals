42085261b9e898e6f82174339bbe9eb5bec5a21e
Radu Matei <matei.radu94@gmail.com>
2016-03-24 16:33:09 +0200

    Added additional level of inheritance for Dog

---
+++ CSharpFundamentals/csharp06 - Inheritance/HuntingDog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Inheritance
+{
+    public class HuntingDog : Dog
+    {
+        public double Speed { get; set; }
+
+        public void Hunt()
+        {
+            Console.WriteLine("{0} {1} hunting dog hunting with {2} mph speed", Color, Breed, Speed);
+        }
+
+        public HuntingDog()
+        {
+        }
+
+        public HuntingDog(string color, string breed, double speed) : base(color, breed)
+        {
+            Speed = speed;
+        }
+    }
+}
