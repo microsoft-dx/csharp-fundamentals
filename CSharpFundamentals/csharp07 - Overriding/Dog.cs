2207139bd9b57e330939c1a4285c511f56f9e6bd
Radu Matei <matei.radu94@gmail.com>
2016-03-24 15:52:37 +0200

    Added Snake and Dog classes that inherit Animal

---
+++ CSharpFundamentals/csharp07 - Overriding/Dog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Overriding
+{
+    public class Dog : Animal
+    {
+        public string Breed { get; set; }
+
+        public override void Eat(string food)
+        {
+            Console.WriteLine("{0} {1} dog eating {2}", Color, Breed, food);
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine("{0} {1} dog barking", Color, Breed);
+        }
+
+        public Dog()
+        {
+        }
+
+        public Dog(string color, string breed) : base(color)
+        {
+            Breed = breed;
+        }
+    }
+}
---
+++ CSharpFundamentals/csharp07 - Overriding/Snake.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Overriding
+{
+    public class Snake : Animal
+    {
+        public bool IsVenomous { get; set; }
+
+        public override void Eat(string food)
+        {
+            string eat = (IsVenomous) ? String.Format("{0} venomous snake eating {1}", Color, food) : String.Format("harmless {0} snake eating {1}", Color, food);
+            Console.WriteLine(eat);
+        }
+
+        public void Bite()
+        {
+            string bite = (IsVenomous) ? String.Format("{0} venomous snake biting", Color) : String.Format("harmless {0} snake biting", Color);
+            Console.WriteLine(bite);
+        }
+
+        public Snake()
+        {
+        }
+
+        public Snake(string color, bool isVenomous) : base(color)
+        {
+            IsVenomous = isVenomous;
+        }
+    }
+}
