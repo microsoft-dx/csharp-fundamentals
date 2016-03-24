99414fdeef8a08d7274353b8a0447dde8ca222de
Radu Matei <matei.radu94@gmail.com>
2016-03-24 15:53:02 +0200

    Added example usage for override

---
+++ CSharpFundamentals/csharp07 - Overriding/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Overriding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal("green");
+            animal.Eat("food");
+
+            Dog dog = new Dog("blue", "bichon");
+            dog.Eat("bones");
+            dog.Bark();
+
+            Snake snake = new Snake("yellow", false);
+            snake.Eat("rats");
+            snake.Bite();
+
+            Console.WriteLine("Array of animals eating: ");
+
+            Animal[] animals = new Animal[3];
+            animals[0] = animal;
+            animals[1] = dog;
+            animals[2] = snake;
+
+            foreach (var a in animals)
+                a.Eat("food for everybody");
+        }
+    }
+}
