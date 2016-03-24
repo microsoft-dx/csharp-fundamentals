ad2676c901f60618a7f12de055feb67703201d96
Radu Matei <matei.radu94@gmail.com>
2016-03-24 15:04:01 +0200

    Added basic class example

---
+++ CSharpFundamentals/csharp05 - Classes/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Classes
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Person()
+        {
+        }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public void PrintPerson()
+        {
+            Console.WriteLine("First Name: {0}, Last Name: {1}, Age: {2}", FirstName, LastName, Age);
+        }
+    }
+}
---
+++ CSharpFundamentals/csharp05 - Classes/Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person newton = new Person("Isaac", "Newton", 373);
+            newton.PrintPerson();
+
+            Person galileo = new Person("Galileo", "Galilei", 452);
+            galileo.PrintPerson();
+        }
+    }
+}
