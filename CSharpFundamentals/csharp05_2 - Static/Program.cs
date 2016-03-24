c0c4cd1a0111618399293a77898dfe4279f105b5
Radu Matei <matei.radu94@gmail.com>
2016-03-24 16:45:18 +0200

    Added example usage

---
+++ CSharpFundamentals/csharp05_2 - Static/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person.PrintCount();
+
+            Person newton = new Person(firstName: "Isaac", lastName: "Newton", age: 373);
+            newton.PrintPerson();
+
+            Person.PrintCount();
+
+            Person galileo = new Person("Galileo", "Galilei", 452);
+            galileo.PrintPerson();
+
+            Person.PrintCount();
+        }
+    }
+}
