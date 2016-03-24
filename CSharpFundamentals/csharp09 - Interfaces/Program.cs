9755118ab3344d4817ed69c3cf41e7b6c82e48d7
Radu Matei <matei.radu94@gmail.com>
2016-03-25 00:51:41 +0200

    Added example

---
+++ CSharpFundamentals/csharp09 - Interfaces/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IVehicle[] vehicles = new IVehicle[]
+            {
+                new Car(),
+                new Plane()
+            };
+
+            IFlyable[] flyingThings = new IFlyable[]
+            {
+                new Plane(),
+                new Bird()
+            };
+
+            Console.WriteLine("Iterating through vehicles");
+
+            foreach (var v in vehicles)
+                v.Move();
+
+            Console.WriteLine("Iterating through flying things");
+
+            foreach (var f in flyingThings)
+                f.Fly();
+        }
+    }
+}
