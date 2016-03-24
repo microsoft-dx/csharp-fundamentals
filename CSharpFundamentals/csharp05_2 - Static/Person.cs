cd8249202be49bee007eaf417d3dd98c6c7b8ada
Radu Matei <matei.radu94@gmail.com>
2016-03-24 16:45:09 +0200

    Added Person class with a sttic property and a static method

---
+++ CSharpFundamentals/csharp05_2 - Static/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Static
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public static int Count { get; private set; }
+
+        public Person()
+        {
+            Count++;
+        }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+
+            Count++;
+        }
+
+        public void PrintPerson()
+        {
+            Console.WriteLine("First Name: {0}, Last Name: {1}, Age: {2}", FirstName, LastName, Age);
+        }
+
+        public static void PrintCount()
+        {
+            Console.WriteLine("Number of persons: {0}", Count);
+        }
+    }
+}
