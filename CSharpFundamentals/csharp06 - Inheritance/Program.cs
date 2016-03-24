e242eb9d931f7fd56cd2b974ff767d95c2667dcc
Radu Matei <matei.radu94@gmail.com>
2016-03-24 15:36:57 +0200

    Added project for inhertitance

---
+++ CSharpFundamentals/csharp06 - Inheritance/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal("green");
+            animal.Eat("food");
+
+            Dog dog = new Dog("blue", "bichon");
+            dog.Eat("bones");
+            dog.Bark();
+
+            Snake snake = new Snake("yellow", false);
+            snake.Bite();
+        }
+    }
+}
