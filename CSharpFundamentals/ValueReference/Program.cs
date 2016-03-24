c55fafb633617ecb0ac7d4208951af74ca93d163
Radu Matei <matei.radu94@gmail.com>
2016-03-24 14:01:45 +0200

    Added example to illustrate passing parameters by value and by reference

---
+++ CSharpFundamentals/ValueReference/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace ValueReference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+            Console.WriteLine("Initially, the value of the number is: {0}", number);
+
+            ModifyNumber(number);
+            Console.WriteLine("After calling the ModifyNumber method, the value of number is: {0}", number);
+
+            int[] numbers = new int[5];
+            Console.WriteLine("Initially, the value of numbers[0] is: {0}", numbers[0]);
+
+            ModifyArray(numbers);
+            Console.WriteLine("After calling the ModifyArray method, the value of numbers[0] is :{0}", numbers[0]);
+        }
+
+        public static void ModifyArray(int[] array)
+        {
+            array[0] = 100;
+        }
+
+        public static void ModifyNumber(int number)
+        {
+            number = 1000;
+        }
+    }
+}
