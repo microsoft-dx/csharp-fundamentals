f12755ff35b58450723dd7e21c39b6f6fe11d5c1
Radu Matei <matei.radu94@gmail.com>
2016-03-24 15:52:23 +0200

    Added Animal class

---
+++ CSharpFundamentals/csharp07 - Overriding/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Overriding
+{
+    public class Animal
+    {
+        public string Color { get; set; }
+
+        public virtual void Eat(string food)
+        {
+            Console.WriteLine("{0} animal eating {1}", Color, food);
+        }
+
+        public Animal()
+        {
+        }
+
+        public Animal(string color)
+        {
+            Color = color;
+        }
+    }
+}
