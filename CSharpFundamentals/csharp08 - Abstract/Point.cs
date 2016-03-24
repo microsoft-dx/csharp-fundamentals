e181415e9ad82b59add12a84c2e65a0b7784e567
Radu Matei <matei.radu94@gmail.com>
2016-03-24 16:21:08 +0200

    Added Point class

---
+++ CSharpFundamentals/csharp08 - Abstract/Point.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Abstract
+{
+    public class Point
+    {
+        public double XCoordinate { get; set; }
+        public double YCoordinate { get; set; }
+
+        public Point()
+        {
+        }
+
+        public Point(double xCoordinate, double yCoordinate)
+        {
+            XCoordinate = xCoordinate;
+            YCoordinate = yCoordinate;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("({0}, {1})", XCoordinate, YCoordinate);
+        }
+    }
+}
