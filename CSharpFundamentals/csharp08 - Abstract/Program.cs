bbf422f426e885531bd2193f37f4bfa3cfbeb8e9
Radu Matei <matei.radu94@gmail.com>
2016-03-24 16:22:34 +0200

    Added project example

---
+++ CSharpFundamentals/csharp08 - Abstract/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape[] shapes = new Shape[] 
+            {
+                new Circle(xCoordinate: 1, yCoordinate: 2, radius: 4),
+                new Square(xCoordinate: 3.5, yCoordinate: 4.6, side: 12)
+            };
+
+            foreach (var shape in shapes)
+                shape.Print();
+        }
+    }
+}
