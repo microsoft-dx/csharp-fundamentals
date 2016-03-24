76c650309164016451079f2bd5f2979312bc20ce
Radu Matei <matei.radu94@gmail.com>
2016-03-24 14:45:40 +0200

    Added method overloading example

---
+++ CSharpFundamentals/csharp04 - MethodOverloading/Program.cs
@@ -0,0 +1,35 @@
+﻿
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Add(2, 3);
+            Add(2, 3, 4);
+
+            Add(2.0, 3.0);
+            Add(2.0, 3.0, 4.0);
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Add(double a, double b, double c)
+        {
+            return a + b + c;
+        }
+    }
+}
