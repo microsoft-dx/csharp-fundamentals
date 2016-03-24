9098fb38947aa8f942f43273424f9db0fe62b687
Radu Matei <matei.radu94@gmail.com>
2016-03-24 15:37:05 +0200

    Added Animal class

---
+++ CSharpFundamentals/csharp06 - Inheritance/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Inheritance
+{
+    public class Animal
+    {
+        public string Color { get; set; }
+
+        public void Eat(string food)
+        {
+            Console.WriteLine("{0} animal eating {1}", Color, food);
+        }
+
+        public Animal()
+        {
+        }
+
+        public Animal(string color)
+        {
+            Color = color;
+        }
+    }
+}
