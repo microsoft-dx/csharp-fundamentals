899e23120cbc1d3b5570edb02330450ec7cce969
Radu Matei <matei.radu94@gmail.com>
2016-03-24 13:42:30 +0200

    Added ref keyword example

---
+++ CSharpFundamentals/RefKeyword/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RefKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 3;
+            Increment(number);
+            Console.WriteLine(number);
+
+            int num = 0;
+            ReferenceIncrement(ref num);
+            Console.WriteLine(num);
+        }
+
+        public static void Increment(int number)
+        {
+            number = number + 1;
+        }
+
+        public static void ReferenceIncrement(ref int number)
+        {
+            number = number + 1;
+        }
+    }
+}
+
