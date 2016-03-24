39605cd453f81b25b5df0215ea4244cf11d96e5b
Radu Matei <matei.radu94@gmail.com>
2016-03-24 15:37:21 +0200

    Added Dog and Snake classes that inherit from Animal

---
+++ CSharpFundamentals/csharp06 - Inheritance/Dog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Inheritance
+{
+    public class Dog : Animal
+    {
+        public string Breed { get; set; }
+
+        public void Bark()
+        {
+            Console.WriteLine("{0} {1} dog barking", Color, Breed);
+        }
+
+        public Dog()
+        {
+        }
+
+        public Dog(string color, string breed) : base(color)
+        {
+            Breed = breed;
+        }
+    }
+}
---
+++ CSharpFundamentals/csharp06 - Inheritance/Snake.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Inheritance
+{
+    public class Snake : Animal
+    {
+        public bool IsVenomous { get; set; }
+
+        public void Bite()
+        {
+            string bite = (IsVenomous) ? String.Format("{0} venomous snake biting") : String.Format("harmless {0} snake biting", Color);
+            Console.WriteLine(bite);
+        }
+
+        public Snake()
+        {
+        }
+
+        public Snake(string color, bool isVenomous) : base(color)
+        {
+            IsVenomous = isVenomous;
+        }
+    }
+}
