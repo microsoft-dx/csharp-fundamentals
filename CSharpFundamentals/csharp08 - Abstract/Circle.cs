b853e2bc837c3edfcfcbc5f4c2e300a1f3ed1086
Radu Matei <matei.radu94@gmail.com>
2016-03-24 16:21:43 +0200

    Added Circle and Square classes that inherit Shape

---
+++ CSharpFundamentals/csharp08 - Abstract/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Abstract
+{
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("This is a circle centered in {0} with {1} radius and with {2} area", Center.ToString(), Radius, GetArea());
+        }
+
+        public Circle()
+        {
+        }
+
+        public Circle(double xCoordinate, double yCoordinate, double radius) : base(xCoordinate, yCoordinate)
+        {
+            Radius = radius;
+        }
+    }
+}
---
+++ CSharpFundamentals/csharp08 - Abstract/Square.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Abstract
+{
+    public class Square : Shape
+    {
+        public double Side { get; set; }
+
+        public override double GetArea()
+        {
+            return Side * Side;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("This is a square centered in {0} with {1} side and with {2} area", Center.ToString(), Side, GetArea());
+        }
+
+        public Square()
+        {
+        }
+
+        public Square(double xCoordinate, double yCoordinate, double side) : base(xCoordinate, yCoordinate)
+        {
+            Side = side;
+        }
+    }
+}
