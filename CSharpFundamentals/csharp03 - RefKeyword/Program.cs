5ae25dfbb9ba8836825808de8a74f74abd78e8d1
Radu Matei <matei.radu94@gmail.com>
2016-03-24 14:33:30 +0200

    Added messages

--- CSharpFundamentals/csharp03 - RefKeyword/Program.cs
+++ CSharpFundamentals/csharp03 - RefKeyword/Program.cs
@@ -8,11 +8,11 @@ namespace RefKeyword
         {
             int number = 3;
             Increment(number);
-            Console.WriteLine(number);
+            Console.WriteLine("After calling the Increment method, the value of number is: {0}", number);
 
             int num = 0;
             ReferenceIncrement(ref num);
-            Console.WriteLine(num);
+            Console.WriteLine("After calling the ReferenceIncrement method, the value of num is: {0}", num);
         }
 
         public static void Increment(int number)
