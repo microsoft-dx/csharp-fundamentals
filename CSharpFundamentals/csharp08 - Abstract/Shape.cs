b9cfcba3dd8c0c9ae9b59288cd380c648a835370
Radu Matei <matei.radu94@gmail.com>
2016-03-24 16:21:21 +0200

    Added abstract Shape class

---
+++ CSharpFundamentals/csharp08 - Abstract/Shape.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Abstract
+{
+    public abstract class Shape
+    {
+        public Point Center { get; set; }
+
+        public abstract double GetArea();
+        
+        public virtual void Print()
+        {
+            Console.WriteLine("This is a shape centered in {0}", Center.ToString());
+        }
+
+        public Shape()
+        {
+        }
+
+        public Shape(double xCoordinate, double yCoordinate)
+        {
+            Center = new Point(xCoordinate, yCoordinate);
+        }
+    }
+}
