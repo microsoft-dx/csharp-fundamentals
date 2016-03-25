53324d454f386413d342783b27ee6baefc045b7e
Radu Matei <matei.radu94@gmail.com>
2016-03-25 17:47:07 +0200

    Added Product class that inherits the Model abstract class and implements the IUptadeable interface

---
+++ CSharpFundamentals/csharp11 - AdvancedGenericsInterfaces/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AdvancedGenericsInterfaces
+{
+    public class Product : Model, IUptadeable<Product>
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Price { get; set; }
+
+        public void Update(Product product)
+        {
+            Id = product.Id;
+            Name = product.Name;
+            Category = product.Category;
+            Price = product.Price;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Id: {0}, Name: {1}, Category: {2}, Price: {3}", Id, Name, Category, Price);
+
+        }
+    }
+}
