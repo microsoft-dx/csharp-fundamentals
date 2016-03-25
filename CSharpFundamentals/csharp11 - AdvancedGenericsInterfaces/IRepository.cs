0e291d736d23904307e3f07e971e071c2a28f64b
Radu Matei <matei.radu94@gmail.com>
2016-03-25 17:47:21 +0200

    Added IRepository interface

---
+++ CSharpFundamentals/csharp11 - AdvancedGenericsInterfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace AdvancedGenericsInterfaces
+{
+    public interface IRepository<TModel> where TModel : Model
+    {
+        void Add(TModel model);
+        TModel GetById(int id);
+        List<TModel> GetAll();
+        void Update(TModel model);
+        void Delete(int id);
+    }
+}
