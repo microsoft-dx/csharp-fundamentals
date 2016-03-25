423ee8e5573342bd33b3f2d752a5120934c85552
Radu Matei <matei.radu94@gmail.com>
2016-03-25 17:03:37 +0200

    Added generic class example

---
+++ CSharpFundamentals/csharp10 - Generics/Generic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Generics
+{
+    public class Generic<T>
+    {
+        public T Property { get; set; }
+
+        public Generic()
+        {
+        }
+
+        public Generic(T property)
+        {
+            Property = property;
+        }
+
+        public override string ToString()
+        {
+            return Property.ToString();
+        }
+    }
+}
