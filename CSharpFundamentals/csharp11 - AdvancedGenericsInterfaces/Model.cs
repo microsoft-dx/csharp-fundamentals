86c0e0eab92b94491f4e437627102a3c904e8224
Radu Matei <matei.radu94@gmail.com>
2016-03-25 17:46:31 +0200

    Added Model abstract class

---
+++ CSharpFundamentals/csharp11 - AdvancedGenericsInterfaces/Model.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AdvancedGenericsInterfaces
+{
+    public abstract class Model : IModel
+    {
+        public int Id { get; set; }
+
+        public abstract void Print();
+    }
+}
