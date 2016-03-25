e203972c7d8c59ae192e9b88e09105818c4ade44
Radu Matei <matei.radu94@gmail.com>
2016-03-25 17:47:52 +0200

    Added generic Repositry class that implements IRepository

---
+++ CSharpFundamentals/csharp11 - AdvancedGenericsInterfaces/Repository.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdvancedGenericsInterfaces
+{
+    public class Repository<TModel> : IRepository<TModel> where TModel : Model, IUptadeable<TModel>
+    {
+        private List<TModel> Entities { get; set; }
+
+        public void Add(TModel model)
+        {
+            Entities.Add(model);
+        }
+
+        public void Delete(int id)
+        {
+            var modelToDelete = Entities.FirstOrDefault(m => m.Id == id);
+
+            if (modelToDelete != null)
+                Entities.Remove(modelToDelete);
+        }
+
+        public TModel GetById(int id)
+        {
+            return Entities.FirstOrDefault(m => m.Id == id);
+        }
+
+        public List<TModel> GetAll()
+        {
+            return Entities;
+        }
+
+        public void Update(TModel model)
+        {
+            var modelToUpdate = Entities.FirstOrDefault(m => m.Id == model.Id);
+
+            if (modelToUpdate != null)
+                modelToUpdate.Update(model);
+        }
+
+        public Repository()
+        {
+            Entities = new List<TModel>();
+        }
+    }
+}
