e334ae99592d9883daf60ec70248ffbd0b326566
Radu Matei <matei.radu94@gmail.com>
2016-03-25 17:48:11 +0200

    Added basic example with repository of Product type

---
+++ CSharpFundamentals/csharp11 - AdvancedGenericsInterfaces/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AdvancedGenericsInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var repo = new Repository<Product>();
+
+            repo.Add(new Product()
+            {
+                Id = 1,
+                Name = "Onion",
+                Category = "Food",
+                Price = 46
+            });
+
+            repo.Add(new Product()
+            {
+                Id = 2,
+                Name = "Carrot",
+                Category = "Food",
+                Price = 23
+            });
+
+            foreach (var p in repo.GetAll())
+            {
+                p.Print();
+            }
+        }
+    }
+}
