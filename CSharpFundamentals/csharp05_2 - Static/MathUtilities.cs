8f9507d676c37fc535bd3839be510d4a665f8a9a
Radu Matei <matei.radu94@gmail.com>
2016-03-29 16:23:24 +0300

    Added method in MathUtilities

--- CSharpFundamentals/csharp05_2 - Static/MathUtilities.cs
+++ CSharpFundamentals/csharp05_2 - Static/MathUtilities.cs
@@ -1,13 +1,15 @@
-﻿using System;
-
-
-namespace Static
+﻿namespace Static
 {
     public static class MathUtilities
     {
         public static int Square(int number)
         {
             return number * number;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
         }
     }
 }
